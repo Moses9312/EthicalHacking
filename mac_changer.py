73622d4d9474a7c7f3130a96eef850503d9d7ae1
root <moise.cristian20@gmail.com>
2024-06-18 15:42:57 -0500

    improve script

--- mac_changer.py
+++ mac_changer.py
@@ -6,6 +6,6 @@ new_mac = input("new MAC > ")
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 
-sub.call("ifconfig " + interface + " down", shell=True)
-sub.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-sub.call("ifconfig " + interface + " up", shell=True)
+sub.call(["ifconfig", interface, "down"])
+sub.call(["ifconfig", interface, "hw", "ether", new_mac])
+sub.call(["ifconfig", interface, "up"])
