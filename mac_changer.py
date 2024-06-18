84831f15d4cd47c56b9e3b27e338401c5f136881
root <moise.cristian20@gmail.com>
2024-06-18 15:45:37 -0500

    improve script

--- mac_changer.py
+++ mac_changer.py
@@ -3,7 +3,7 @@ import subprocess as sub
 print(sub.call("ifconfig"))
 interface = input("interface > ")
 new_mac = input("new MAC > ")
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
 
 sub.call(["ifconfig", interface, "down"])
