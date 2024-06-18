a60df52188e916d932f8dbdc2fea5d2eec3d599a
root <moise.cristian20@gmail.com>
2024-06-18 15:22:52 -0500

    create basic macchanger script

--- mac_changer.py
+++ mac_changer.py
@@ -1,10 +1,10 @@
 import subprocess as sub
 
 interface = "eth0"
-new_mac = "00:11:22:33:44:77"
+new_mac = "00:11:22:33:44:88"
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 
-sub.call(f"ifconfig {interface}", shell=True)
-sub.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
-sub.call(f"ifconfig {interface} up", shell=True)
+sub.call("ifconfig " + interface + " down", shell=True)
+sub.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+sub.call("ifconfig " + interface + " up", shell=True)
